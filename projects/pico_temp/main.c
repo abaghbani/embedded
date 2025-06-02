b8b28c2f1433085e176f7e7eede9932b9ff6916b
Akbar Baghbani <abaghbani@gmail.com>
2025-06-02 09:33:21 +0200

    [PICO_TEMP] log print over usb/uart is added

--- projects/pico_temp/main.c
+++ projects/pico_temp/main.c
@@ -1,10 +1,15 @@
+#include <stdio.h>
 #include "pico/stdlib.h"
 
 int main() {
     const uint LED_PIN = 25;
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    stdio_init_all();
+    printf("Hello, Raspberry Pi Pico!\n");
     while (1) {
+        printf("Blinking LED on pin %d\n", LED_PIN);
         gpio_put(LED_PIN, 1);
         sleep_ms(500);
         gpio_put(LED_PIN, 0);
